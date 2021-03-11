9f066a6b4b7a2b7ab8eb7c4f18e9dce628382e65
Jan Kuliga <jan@localhost.localdomain>
2021-03-12 00:17:29 +0100

    test expressions

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include "stdlib.h"
+#include "linked-lists/single_linked_list.c"
+#include "arrays/array.c"
+
+
 
 int main(void)
 {
-        
+        int arr[] = { 10, 20, 180, 30, 60, 50, 110, 100, 70 };
+        int inorder[] = {11 ,20, 30, 50, 60, 70, 100, 110, 180};
+        int n = sizeof(arr) / sizeof(arr[0]);
+        int key = 180;
+        int x = search(arr, key, n);
+        x = sentinel_search(arr, key, n);
+        x = binary_search(inorder, 20, n);
+        int tmp = 0;
+
+        return 0;
 }
