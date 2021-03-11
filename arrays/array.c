396fb6fb46fba71d11e700eea2ac2d8a3426d1c4
Jan Kuliga <jan@localhost.localdomain>
2021-03-12 00:15:46 +0100

    binary_search() ok

--- arrays/array.c
+++ arrays/array.c
@@ -3,29 +3,50 @@
 #define N       100
 #define NOVAL   -1
 
-int arr[N];
 
 /*tablice nieuporządkowane == kolejność elementów nieznana*/
 int search(int *arr, int val, size_t n)
 {
-        // for (short i = 0; i < n; i++) {
-        //         if (*(arr + i) == val)
-        //                 return i;
-        // }
-        // return NOVAL;
-        short i = 0;
-        while 
+        for (short i = 0; i < n; i++) {
+                if (arr[i] == val)
+                        return i;
+        }
+        return NOVAL;
 }
 
-int search_sentinel(int *arr, int val, size_t n)
+int sentinel_search(int *arr, int val, size_t n)
 {
-        // *(arr + n) = val;
-        arr[n] = val;
+        int tmp = arr[n - 1];
+        arr[n - 1] = val;
         short i = 0;
         while (arr[i] != val)
                 i++;
 
+        arr[n - 1] = tmp;
         if (i < n)
                 return i;
+        return NOVAL;
+}
+
+/*tablice uporządkowane*/
+//tablica uporządkowana niemalejąkco*/
+int binary_search(int *arr, int val, size_t n)
+{
+        short l, r, k; //lewa prawa granica, kolejny indeks
+        l = 0;
+        r = n - 1;
+        k = n / 2;
+        int tmp;
+        while (l <= r) { 
+                k = (l + r) / 2;
+                tmp = arr[k];
+                if (tmp == val)
+                        return k;
+        
+                if (tmp < val)
+                        l = k + 1;
+                else 
+                        r  = k - 1;
+        }
         return NOVAL;
 }
