5f1074b6a719604a23e40f0ea54553bb242647ab
Jan Kuliga <jan@localhost.localdomain>
2021-03-23 22:03:50 +0100

    binary_search() ok

--- linked-lists/single_linked_list.c
+++ linked-lists/single_linked_list.c
@@ -1,8 +1,13 @@
 #include "stdlib.h"
 #include "stdint.h"
 
-struct node_of_sll {
+struct node_sll {
+        struct node_sll *next;
         uint32_t val;
-        struct node_of_sll *next;
 };
+
+void sll_push(struct node_sll *p, uint32_t val)
+{
+        
+}
 
